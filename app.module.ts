67f36b07e1991a32b2b3de153edcbce12c724cb4
swethasureshkumarssk14 <61614493+swethasureshkumarssk14@users.noreply.github.com>
2020-10-07 19:24:53 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule } from '@angular/forms';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    LoginsuccessComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ registration.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  constructor(private _http : HttpClient) { }
+
+  public loginUserFromRemote(user :User):Observable<any>{
+    return this._http.post("http://localhost:8090/login",user)
+  }
+}
